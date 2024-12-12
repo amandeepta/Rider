0e86b17e903e84b129c3f6bce81a104229b30158
amandeepta <amandiptaitis@gmail.com>
2024-12-12 16:42:30 +0530

    A small change

--- server/controller/search.js
+++ server/controller/search.js
@@ -17,8 +17,7 @@ const Search = async (req, res) => {
                  message: 'No rides were found for the given location' });
         }
 
-        return res.status(200).json({ success : true, 
-            rides: data });
+        return res.status(200).json({ success : true, data });
     } catch (error) {
         return res.status(500).json({success : false, message: 'An error occurred while searching for rides', error: error.message });
     }
